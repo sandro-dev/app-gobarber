a7c9b0141bfa11343a73fe7710d3df6f7a89b86f
Sandro Santos <sandro.dev@gmail.com>
2021-02-09 14:34:47 -0300

    feat: initial config of app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>App Gobarber</Text>
+    </View>
+  );
+};
+
+export default App;
