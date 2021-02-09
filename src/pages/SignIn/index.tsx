3beb16ddee5ad9a577cb8ec69b54c8c30b4b6ed5
Sandro Santos <sandro.dev@gmail.com>
2021-02-09 20:47:46 -0300

    feat: SignIn page Inputs and Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import logoImg from '../../assets/logo.png';
 
 import { Container, Text } from './styles';
@@ -8,7 +11,13 @@ import { Container, Text } from './styles';
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
-    <Text>SignIn page</Text>
+    <Text>Faça seu login</Text>
+
+    <Input name="email" icon="mail" placeholder="E-mail" />
+    <Input name="password" icon="lock" placeholder="Senha" />
+
+    <Button onPress={() => { console.log('Enter'); }}>Entrar</Button>
+
   </Container>
 );
 
