bf80db9bc0926cf58b73b03b24c1bb245975785c
Sandro Santos <sandro.dev@gmail.com>
2021-02-09 16:55:45 -0300

    feat: basic structure of pages SignIn and SignUp

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Text>SignIn page</Text>
+  </Container>
+);
+
+export default SignIn;
