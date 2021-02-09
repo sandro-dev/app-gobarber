a6a0af005e3b7b68b423bebb8f70b44b02c75a44
Sandro Santos <sandro.dev@gmail.com>
2021-02-09 17:04:50 -0300

    feat: header style

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,15 @@ import SignUp from '../pages/SignUp';
 const Auth = createStackNavigator();
 
 const AuthRoutes: React.FC = () => (
-  <Auth.Navigator>
+  <Auth.Navigator screenOptions={{
+    // headerShown: false,
+    headerTintColor: '#fff',
+    headerStyle: {
+      backgroundColor: '#74b9ff',
+    },
+    cardStyle: { backgroundColor: '#312e38' },
+  }}
+  >
     <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
   </Auth.Navigator>
