2a3804fe35b36be05c6ce3dabda2293ce9ef7c70
Sandro Santos <sandro.dev@gmail.com>
2021-02-10 14:01:54 -0300

    feat: initial route SignUp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import SignIn from '../pages/SignIn';
 
 const Auth = createStackNavigator();
 
 const AuthRoutes: React.FC = () => (
-  <Auth.Navigator screenOptions={{
-    headerShown: false,
-    // headerTintColor: '#fff',
-    // headerStyle: {
-    //   backgroundColor: '#74b9ff',
-    // },
-    cardStyle: { backgroundColor: '#312e38' },
-  }}
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      // headerTintColor: '#fff',
+      // headerStyle: {
+      //   backgroundColor: '#74b9ff',
+      // },
+      cardStyle: { backgroundColor: '#312e38' },
+    }}
+    initialRouteName="SignUp"
   >
-    <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
+    <Auth.Screen name="SignIn" component={SignIn} />
   </Auth.Navigator>
 );
 
